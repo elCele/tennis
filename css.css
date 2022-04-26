33de70e55ddfa4963ce138f8f4788b70551eee23
elCele <102757504+elCele@users.noreply.github.com>
2022-04-26 23:00:01 +0200

    site creating

---
+++ css.css
@@ -0,0 +1,53 @@
+table {
+    width: 99vw;
+    text-align: center;
+    height: 25vh;
+}
+
+.first, .second, .third, #p1, #p2 {
+    background-color: rgb(119, 96, 140);
+    padding: 5px;
+    border-radius: 15px;    
+}
+
+.fg {
+    background-color: rgb(0, 191, 16);
+    padding: 20px;
+    border-radius: 15px;
+}
+
+.point {
+    background-color: rgb(188, 0, 0);
+    padding: 20px;
+    border-radius: 15px;
+}
+
+div {
+    width: auto;
+    height: auto;
+    margin: 5px;
+    border-radius: 15px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+}
+
+.typeShoot {
+    background-color: rgb(156, 49, 255);
+    color: white;
+}
+
+.placeShoot {
+    background-color: rgb(186, 150, 220);
+}
+
+button {
+    border-radius: 15px;
+    height: 100%;
+    width: 100%;
+}
+
+#P2_W, #P1_W {
+    padding: 20px;
+    background-color: rgb(255, 238, 0);
+    border-radius: 15px;
+}
