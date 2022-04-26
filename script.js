f5e5d14d3021f754a975e5cae2f5a6c0455fcee4
elCele <102757504+elCele@users.noreply.github.com>
2022-04-26 23:00:39 +0200

    js adjust

--- script.js
+++ script.js
@@ -1,53 +1,107 @@
-table {
-    width: 99vw;
-    text-align: center;
-    height: 25vh;
+const turnObj = {
+    smash: '',
+    first: '',
+    second: '',
+    third: '',
+    exchange: '',
+    FoG: '',
+    winner: ''
+};
+
+const turn = new Object(turnObj);
+let match = [];
+
+/* Player 1 variables
+ quante battute di prima
+ ''     ''      '' seconda
+ quanti battute centrali, esterne o interne [cP1_F, eP1_F, iP1_F]  | battute totali
+ quante risposte centrali, sulla t, diagonali e lungolinea [cP1_S, tP1_S, dP1_S, lP1_S]    | risposte totali
+ quanti terzi tocchi centrali, sulla t, diagonali e lungolinea [cP1_T, tP1_T, dP1_T, lP1_T]    | terzi tocchi totali
+ quanti forzati [nF]
+ quanti gratuiti [nG]
+ quanti match vinti [nW]
+
+ idem per il player 2
+
+ battute totali
+ risposte totali
+ terzi colpi totali
+
+ quanti match tra 0-4, tra 4-8 e 9+
+ match totali
+ */
+
+function reset() {
+    turn.smash = '';
+    turn.first = '';
+    turn.second = '';
+    turn.third = '';
+    turn.FoG = '';
+    turn.exchange = '';
+    turn.winner = '';
+    
+
+    console.log(turn);
 }
 
-.first, .second, .third, #p1, #p2 {
-    background-color: rgb(119, 96, 140);
-    padding: 5px;
-    border-radius: 15px;    
+function smash(elementID) {
+    turn.smash = elementID;
+    
+    console.log(turn);
 }
 
-.fg {
-    background-color: rgb(0, 191, 16);
-    padding: 20px;
-    border-radius: 15px;
+function first(elementID) {
+    turn.first = elementID;
+    
+    console.log(turn);
 }
 
-.point {
-    background-color: rgb(188, 0, 0);
-    padding: 20px;
-    border-radius: 15px;
+function second(elementID) {
+    turn.second = elementID;
+    
+    console.log(turn);
 }
 
-div {
-    width: auto;
-    height: auto;
-    margin: 5px;
-    border-radius: 15px;
-    padding-top: 10px;
-    padding-bottom: 10px;
+function third(elementID) {
+    turn.third = elementID;
+    
+    console.log(turn);
 }
 
-.typeShoot {
-    background-color: rgb(156, 49, 255);
-    color: white;
+function FoG(elementID) {
+    turn.FoG = elementID;
+    
+    console.log(turn);
 }
 
-.placeShoot {
-    background-color: rgb(186, 150, 220);
+function exchange(elementID) {
+    turn.exchange = elementID;
+    
+    console.log(turn);
 }
 
-button {
-    border-radius: 15px;
-    height: 100%;
-    width: 100%;
+function winner(elementID) {
+    turn.winner = elementID;
+    
+    console.log(turn);
 }
 
-#P2_W, #P1_W {
-    padding: 20px;
-    background-color: rgb(255, 238, 0);
-    border-radius: 15px;
+function submit() {
+        match.push(turn.smash);
+        match.push(turn.first);
+        match.push(turn.second);
+        match.push(turn.third);
+        match.push(turn.FoG);
+        match.push(turn.exchange);
+        match.push(turn.winner);
+
+        turn.smash = '';
+        turn.first = '';
+        turn.second = '';
+        turn.third = '';
+        turn.FoG = '';
+        turn.exchange = '';
+        turn.winner = '';
+
+        console.log(match);
 }
