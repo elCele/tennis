c2f7b763d63412c0db633dec5395f07fdfc92823
elCele <102757504+elCele@users.noreply.github.com>
2022-05-04 23:17:08 +0200

    .

--- script.js
+++ script.js
@@ -12,11 +12,106 @@ const punto = {
     vincitore: ''
 }
 
+/* Giocatore 1 
+    batTotG1: document.getElementById('batTotG1'),
+    batPrimaG1: document.getElementById('batPrimaG1'),
+    batSecondaG1: document.getElementById('batSecondaG1'),
+    batNoneG1: document.getElementById('batNoneG1'),
+    batEG1: document.getElementById('batEG1'),
+    batCG1: document.getElementById('batCG1'),
+    batIG1: document.getElementById('batIG1'),
+    rispTotG1: document.getElementById('rispTotG1'),
+    rispNoneG1: document.getElementById('rispNoneG1'),
+    rispCG1: document.getElementById('rispCG1'),
+    rispTG1: document.getElementById('rispTG1'),
+    rispDG1: document.getElementById('rispDG1'),
+    rispLG1: document.getElementById('rispLG1'),
+    t3TotG1: document.getElementById('t3TotG1'),
+    t3NoneG1: document.getElementById('t3NoneG1'),
+    t3CG1: document.getElementById('t3CG1'),
+    t3T1: document.getElementById('t3TG1'),
+    t3D1: document.getElementById('t3DG1'),
+    t3L1: document.getElementById('t3LG1'),
+    winG1: document.getElementById('winG1')
+*/
+
+/* Giocatore 2 
+    batTotG1: document.getElementById('batTotG1'),
+    batPrimaG1: document.getElementById('batPrimaG1'),
+    batSecondaG1: document.getElementById('batSecondaG1'),
+    batNoneG1: document.getElementById('batNoneG1'),
+    batEG1: document.getElementById('batEG1'),
+    batCG1: document.getElementById('batCG1'),
+    batIG1: document.getElementById('batIG1'),
+    rispTotG1: document.getElementById('rispTotG1'),
+    rispNoneG1: document.getElementById('rispNoneG1'),
+    rispCG1: document.getElementById('rispCG1'),
+    rispTG1: document.getElementById('rispTG1'),
+    rispDG1: document.getElementById('rispDG1'),
+    rispLG1: document.getElementById('rispLG1'),
+    t3TotG1: document.getElementById('t3TotG1'),
+    t3NoneG1: document.getElementById('t3NoneG1'),
+    t3CG1: document.getElementById('t3CG1'),
+    t3T1: document.getElementById('t3TG1'),
+    t3D1: document.getElementById('t3DG1'),
+    t3L1: document.getElementById('t3LG1'),
+    winG1: document.getElementById('winG1')
+*/
+//pozzo variabili
 let puntiGameG1 = 0;
 let puntiGameG2 = 0;
 let gameG1 = 0;
 let gameG2 = 0;
 
+//oggetto Giocatore 1
+
+const Giocatore1 = {
+    batTotG1: 0,
+    batPrimaG1: 0,
+    batSecondaG1: 0,
+    batNoneG1: 0,
+    batEG1: 0,
+    batCG1: 0,
+    batIG1: 0,
+    rispTotG1: 0,
+    rispNoneG1: 0,
+    rispCG1: 0,
+    rispTG1: 0,
+    rispDG1: 0,
+    rispLG1: 0,
+    t3TotG1: 0,
+    t3NoneG1: 0,
+    t3CG1: 0,
+    t3T1: 0,
+    t3D1: 0,
+    t3L1: 0,
+    winG1: 0
+}
+
+//oggetto Giocatore 2
+
+const Giocatore2 = {
+    batTotG2: 0,
+    batPrimaG2: 0,
+    batSecondaG2: 0,
+    batNoneG2: 0,
+    batEG2: 0,
+    batCG2: 0,
+    batIG2: 0,
+    rispTotG2: 0,
+    rispNoneG2: 0,
+    rispCG2: 0,
+    rispTG2: 0,
+    rispDG2: 0,
+    rispLG2: 0,
+    t3TotG2: 0,
+    t3NoneG2: 0,
+    t3CG2: 0,
+    t3T2: 0,
+    t3D2: 0,
+    t3L2: 0,
+    winG2: 0
+}
 
 function submitPunto() {
 
