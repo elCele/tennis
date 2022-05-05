333a4ea0660105ffbf76de35f4d84e9472c12376
elCele <102757504+elCele@users.noreply.github.com>
2022-05-05 20:29:27 +0200

    .

--- script.js
+++ script.js
@@ -159,6 +159,7 @@ function submitPunto() {
     punto.giocatoreTT = document.getElementById('playerBattuta').value;
     punto.dirTT = document.getElementById('terzoToccoDir').value;
     punto.fog = document.getElementById('fg').value;
+    console.log(punto.fog);
     punto.durata = document.getElementById('game').value;
     console.log(punto.durata);
     punto.vincitore = document.getElementById('vincitore').value;
